0dfcca3acace5b136bbb4416e64195825cc77b77
Maciej <mackolinop@gmail.com>
2025-10-13 22:33:46 +0200

    Merge local with repo

---
+++ app/src/main/java/com/example/mazeball/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.mazeball
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mazeball.ui.theme.MazeBallTheme
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // added
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        val settingsButton = findViewById<Button>(R.id.settingsButton)
+
+        startButton.setOnClickListener {
+            // przejście do ekranu gry
+            Toast.makeText(this, "Starting game...", Toast.LENGTH_SHORT).show()
+        }
+
+        settingsButton.setOnClickListener {
+            // TODO: dodaj ekran ustawień w przyszłości
+        }
+
+        /*
+        enableEdgeToEdge()
+        setContent {
+            MazeBallTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }*/
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MazeBallTheme {
+        Greeting("Android")
+    }
+}
